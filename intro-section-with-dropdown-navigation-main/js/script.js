7460db1f743af4d4fdb51feb1fbf1500284fc52e
Alexey Rusel <alexeyrusel@gmail.com>
2022-10-13 19:25:42 +0300

    feat: finish functionality on mobile

---
+++ intro-section-with-dropdown-navigation-main/js/script.js
@@ -0,0 +1,24 @@
+let hidden = document.querySelector('.hidden');
+let navButtons = document.querySelectorAll('.nav-button');
+let openNavButton = document.querySelector('.open-nav-button');
+let closeNavButton = document.querySelector('.close-nav-button');
+let nav = document.querySelector('.header__inner');
+
+for(let i = 0; i < navButtons.length; i++) {
+    navButtons[i].addEventListener('click', function() {
+        this.classList.toggle('.active-nav-button');
+        
+
+    if (openNavButton.style.display == "none") {
+        hidden.style.display = "none";
+        nav.style.display = 'none';
+        openNavButton.style.display = "block";
+        closeNavButton.style.display = "none";
+    } else {
+        hidden.style.display = "block";
+        nav.style.display = 'block';
+        openNavButton.style.display = "none";
+        closeNavButton.style.display = "block";
+    }
+    })
+}
