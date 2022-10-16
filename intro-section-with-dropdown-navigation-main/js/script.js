2024cb951dc804023bc2c071b3a8f0d4b2e043c3
Alexey Rusel <alexeyrusel@gmail.com>
2022-10-16 13:48:48 +0300

    fix: disable scroll in menu

--- intro-section-with-dropdown-navigation-main/js/script.js
+++ intro-section-with-dropdown-navigation-main/js/script.js
@@ -3,22 +3,25 @@ let navButtons = document.querySelectorAll('.nav-button');
 let openNavButton = document.querySelector('.open-nav-button');
 let closeNavButton = document.querySelector('.close-nav-button');
 let nav = document.querySelector('.header__inner');
+let body = document.querySelector('body');
 
 for(let i = 0; i < navButtons.length; i++) {
     navButtons[i].addEventListener('click', function() {
         this.classList.toggle('.active-nav-button');
         
 
-    if (openNavButton.style.display == "none") {
-        hidden.style.display = "none";
+    if (openNavButton.style.display == 'none') {
+        hidden.style.display = 'none';
         nav.style.display = 'none';
-        openNavButton.style.display = "block";
-        closeNavButton.style.display = "none";
+        openNavButton.style.display = 'block';
+        closeNavButton.style.display = 'none';
+        body.style.position = 'static';
     } else {
-        hidden.style.display = "block";
+        hidden.style.display = 'block';
         nav.style.display = 'block';
-        openNavButton.style.display = "none";
-        closeNavButton.style.display = "block";
+        openNavButton.style.display = 'none';
+        closeNavButton.style.display = 'block';
+        body.style.position = 'fixed';
     }
     })
 }
