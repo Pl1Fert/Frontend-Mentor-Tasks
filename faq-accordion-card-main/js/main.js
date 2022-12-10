2da909f55ef1bc2b7ab810f099c5bebc56041e9f
Alexey Rusel <alexeyrusel@gmail.com>
2022-12-10 18:57:55 +0300

    fix

--- faq-accordion-card-main/js/main.js
+++ faq-accordion-card-main/js/main.js
@@ -1,14 +1,7 @@
-let acc = document.querySelectorAll('.accordion__item__header');
+let acc = document.querySelectorAll('.accordion__item');
 
 for(let i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
-        this.classList.toggle("accordion__item__show");
-
-        let body = this.nextElementSibling;
-        if(body.style.display === "block") {
-            body.style.display = "none";
-        } else {
-            body.style.display = "block";
-        }
+        this.classList.toggle("accordion__item_show");
     })
 }
